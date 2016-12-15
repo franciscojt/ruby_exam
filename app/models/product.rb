976d5bda22a439a594272d62e2b176cbb88ea805
Francisco J Trujillo <francisco.jose1982@gmail.com>
2016-12-15 14:36:49 -0600

    belt exam

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
 	has_many :boughts, through: :buyers, source: :user, foreign_key: "user_id"
 	validates_presence_of :name, :price
 	after_initialize :init
+	validates_numericality_of :price, greater_than:(0)
 	
 	def init
 		self.bought = false if self.bought.nil?
